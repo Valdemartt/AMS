2d629325c36382bdbd7ef54a614c439f1d8d32e6
Valdemartt <36989028+Valdemartt@users.noreply.github.com>
2020-02-20 10:15:24 +0100

    Added test project for bootloader test

---
+++ LAB6_Bootloading/Test/Test/main.c
@@ -0,0 +1,18 @@
+/*
+ * Test.c
+ *
+ * Created: 20-02-2020 10:11:29
+ * Author : valde
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
